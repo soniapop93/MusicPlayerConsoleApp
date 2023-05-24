8fadba26d2d9c9b5657bb47afb1412ef7e10ee10
soniapop93 <popsonia85@gmail.com>
2023-05-24 21:50:55 +0300

    first commit

---
+++ MusicPlayerConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        /*
+           =============================================================
+           =============================================================
+            
+            MusicPlayerConsoleApp
+
+           [ ] takes path from user where the files are
+           [ ] list all songs
+           [ ] user should select the song by name
+           [ ] can pause songs
+           [ ] can stop songs
+           [ ] will play next song in list
+           [ ] can have option to shuffle the songs
+           [ ] can play song on loop
+           [ ] can exit the player and the song will stop
+        
+           =============================================================
+           =============================================================
+        */
+
+        Console.WriteLine("------------------------ SCRIPT STARTED ------------------------");
+
+       
+
+        Console.WriteLine("------------------------ SCRIPT FINISHED ------------------------");
+    }
+}
