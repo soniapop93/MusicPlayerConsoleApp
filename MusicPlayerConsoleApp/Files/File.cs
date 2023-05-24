fb8c1b49ed59c3926d5d1c07bea1bec2614e3ce7
soniapop93 <popsonia85@gmail.com>
2023-05-24 22:16:42 +0300

    started to implement the logic to get the files

---
+++ MusicPlayerConsoleApp/Files/File.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayerConsoleApp.Files
+{
+    public class File
+    {
+        public string name { get; set; }
+        public string extension { get; set; }
+        public string path { get; set; }
+
+        public File(string name, string extension)
+        {
+            this.name = name;
+            this.extension = extension;
+        }
+    }
+}
--- MusicPlayerConsoleApp/Files/FileHandler.cs
+++ MusicPlayerConsoleApp/Files/FileHandler.cs
@@ -8,7 +8,32 @@ namespace MusicPlayerConsoleApp.Files
 {
     public class FileHandler
     {
+        readonly List<string> extensions = new List<string> {"wav"};
+        public List<File> listAllFiles(string path)
+        {
+            List<File> files = new List<File>();
 
+            foreach (string extension in extensions)
+            {
+                string[] listFiles = Directory.GetFiles(path, extension);
+                
+                for (int i = 0; i < listFiles.Length; i++)
+                {
+                    File file = getInfo(listFiles[i]);
+                    file.path = listFiles[i];
+                    files.Add(file);
+                }
+            }
+            return files;
+        }
+
+        public File getInfo(string filePath)
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+
+            File file = new File(fileInfo.Name, fileInfo.Extension);
+
+            return file;
+        }
     }
 }
-s
